ab8b9be360cf253877e72d793d7a972b38c3781d
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-13 14:36:53 -0400

    Simplify collector registry

--- csat/acquisition/__init__.py
+++ csat/acquisition/__init__.py
@@ -1,24 +1,14 @@
-from __future__ import absolute_import
-
 from twisted import plugin
 
 from csat import collectors
 from csat.acquisition import base
 
 
-_collectors = None
-_models_to_collectors = None
-
-
-def get_collector(hashed):
-    _collectors = get_collectors()
-
-    try:
-        return _collectors[hashed]
-    except KeyError:
-        raise ValueError('Invalid collector')
+def get_collector(key):
+    for c in get_collector():
+        if key == c.key:
+            return c
 
 
 def get_collectors():
-    configurators = plugin.getPlugins(base.ICollectorConfigurator, collectors)
-    return {c.key: c for c in configurators}
+    return plugin.getPlugins(base.ICollectorConfigurator, collectors)
