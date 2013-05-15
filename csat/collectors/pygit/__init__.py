83c9aac5751553476104b290652a37bb9e4dab67
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-15 17:18:04 -0400

    Adding more arguments to the command line parser

--- csat/collectors/pygit/__init__.py
+++ csat/collectors/pygit/__init__.py
@@ -23,13 +23,18 @@ class GitPythonCollector(base.FactoryBase):
     def build_parser(self, base):
         parser = super(GitPythonCollector, self).build_parser(base)
         parser.add_argument('repo_path')
+        parser.add_argument('revspec', default='master', nargs='?')
+        parser.add_argument('package_name')
         return parser
 
     def build_collector(self, task_manager, logger, args):
         from .collector import GitPythonCollector
 
+        #rev: 'trunk' #'63be1f698a60d572fc6436cbc0af9e0fcff9713e..a0626e'
+
         repo = git.Repo(args.repo_path)
-        return GitPythonCollector(task_manager, logger, repo)
+        return GitPythonCollector(task_manager, logger, repo, args.revspec,
+                                  args.package_name)
 
 
 if git is not None:
