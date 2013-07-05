eb2aae2739d0b46c48cd3a6b2aaa638f17379cf3
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-07-05 19:53:21 -0400

    Uninstall the debug toolbar

--- csat/webapp/settings.py
+++ csat/webapp/settings.py
@@ -132,7 +132,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -171,7 +171,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.markup',
-    'debug_toolbar',
     'south',
     'coffin',
     'polymorphic',
