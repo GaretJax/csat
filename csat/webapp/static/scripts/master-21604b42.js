c3a480179917511ddf2923f9510cdb0195ae7880
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-07-06 12:38:59 -0400

    Removed leftovers

--- csat/webapp/static/scripts/master-21604b42.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  return $(function() {
-    $('.flip-box').click(function() {});
-    $(':file').filestyle({
-      icon: true
-    });
-    return $('input[data-slider-value]').slider();
-  });
-})(jQuery);
