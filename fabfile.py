24b9290fe7b7a9f82b4877c03bc0392891f1dd06
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-07-06 16:10:39 -0400

    Working release task.

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ import signal
 import pipes
 
 from fabric.api import env, sudo, cd, settings, prefix, task, local, hide
+from fabric.contrib.console import confirm
 
 
 env.app = {
@@ -210,7 +211,6 @@ def release():
     if not is_working_tree_clean():
         print 'Your working tree is not clean. Refusing to create a release.'
         return
-    return
 
     print 'Rebuilding assets to ensure everything is up to date...'
     assets()
@@ -219,13 +219,23 @@ def release():
         print 'Your working tree is not clean after the assets where rebuilt.'
         print 'Please commit the changes before continuing.'
         return
-    return
 
     # Check version
     version = local('python setup.py --version', capture=True)
 
     # Tag
     tag_message = 'CSAT release version {}.'.format(version)
+
+    print '----------------------'
+    print 'Proceeding will tag the release, push the repository upstream,'
+    print 'and release a new version on PyPI.'
+    print
+    print 'Version: {}'.format(version)
+    print 'Tag message: {}'.format(tag_message)
+    print
+    if not confirm('Continue?', default=True):
+        print 'Aborting.'
+        return
 
     local('git tag -a {} -m {}'.format(pipes.quote(version),
                                        pipes.quote(tag_message)))
@@ -235,5 +245,3 @@ def release():
 
     # Package and upload to pypi
     local('python setup.py sdist upload')
-
-    # Upload to github
