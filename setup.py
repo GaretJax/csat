db63441136369436140a91c9657444353c8944e6
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-07-29 15:59:21 -0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class Setup(object):
 
 setup(
     name='csat',
-    version='0.2.2',
+    version='0.3',
     description='Complex Software Analysis Toolkit',
     author='Jonathan Stoppani',
     author_email='jonathan@stoppani.name',
